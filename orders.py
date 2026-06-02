5334fb7e386761234e4107fe31472834803ca4de
Aditya Raghav <aditya.raghv442@gmail.com>
2026-06-02 17:11:54 +0530

    Update orders.py

--- orders.py
+++ orders.py
@@ -1,34 +1,26 @@
 from bot.client import client
 from bot.logging_config import logger
-
-
 def place_market_order(symbol, side, quantity):
     try:
         logger.info(
             f"MARKET ORDER REQUEST: "
             f"{symbol} {side} {quantity}"
         )
-
         response = client.futures_create_order(
             symbol=symbol,
             side=side,
             type="MARKET",
             quantity=quantity
         )
-
         logger.info(
             f"MARKET RESPONSE: {response}"
         )
-
         return response
-
     except Exception:
         logger.exception(
             "Market order failed"
         )
         raise
-
-
 def place_limit_order(
     symbol,
     side,
@@ -41,7 +33,6 @@ def place_limit_order(
             f"{symbol} {side} "
             f"{quantity} {price}"
         )
-
         response = client.futures_create_order(
             symbol=symbol,
             side=side,
@@ -50,13 +41,10 @@ def place_limit_order(
             price=price,
             timeInForce="GTC"
         )
-
         logger.info(
             f"LIMIT RESPONSE: {response}"
         )
-
         return response
-
     except Exception:
         logger.exception(
             "Limit order failed"
